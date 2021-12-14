f9b83823b00c87ab4b2605c215f5ee4fc484bcb8
Iryna Shandar <iryna.shandar@gmail.com>
2021-12-15 00:18:18 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 // App.js
 import './App.css';
+import Profile from './Components/social-profile/profile';
 
-function App() {
-  return <div className="App">TEST</div>;
-}
+
+// function App() {
+//   return <div className="App">TEST</div>;
+// }
+
+const App = (props) => (
+    <div>
+      <Profile
+        username={props.user.username}
+        tag={props.user.tag}
+        location={props.user.location}
+        avatar={props.user.avatar}
+        stats={props.user.stats}
+      />
+      {/* <Statistics title={props.data.title} stats={props.data} />
+      <FriendList friends={props.friends} />,
+      <TransactionHistory items={props.transactions} /> */}
+    </div>
+  );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import user from "../src/Components/social-profile/user.json"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+    user={user}
+    />
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// import data from "../src/Components/Statistics/data.json";
+// import friends from "../src/Components/FriendList/friends.json";
+// import transactions from "../src/Components/TransactionHistory/transactions.json";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App
+//       user={user}
+//       data={data}
+//       friends={friends}
+//       transactions={transactions}
+//     />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
