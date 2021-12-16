ad6b3be411fd3846bd3bea34b19e9c6e8c3b9bc3
Iryna Shandar <iryna.shandar@gmail.com>
2021-12-17 00:47:26 +0200

    transaction

---
+++ src/Components/TransactionHistory/transactionHistory.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./TransactionHistory.module.css";
+import Transaction from "./Transaction";
+
+const TransactionHistory = ({ items }) => (
+  <table className={styles.transactionHistory}>
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+    <tbody>
+      {items.map((item) => (
+        <Transaction
+          key={item.id}
+          type={item.type}
+          amount={item.amount}
+          currency={item.currency}
+        />
+      ))}
+    </tbody>
+  </table>
+);
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  amount: PropTypes.number,
+  currency: PropTypes.string,
+};
+
+export default TransactionHistory;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import user from "../src/Components/social-profile/user.json"
+import user from "../src/Components/SocialProfile/user.json"
 import data from "../src/Components/statistics/data.json"
+import friends from "../src/Components/FriendList/friend.json"
+import transactions from "../src/Components/TransactionHistory/transactions.json"
 
 ReactDOM.render(
   <React.StrictMode>
     <App 
     user={user}
     data={data}
+    friends={friends}
+    transactions={transactions}
     />
   </React.StrictMode>,
   document.getElementById("root")
